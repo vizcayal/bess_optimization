6c34dc53922b73010eba2fba0d261b7f7c152a07
Luis Vizcaya <luis.vizcaya@woodmac.com>
2025-01-26 20:24:36 -0600

    comment of functions improved

--- src/bess.py
+++ src/bess.py
@@ -2,23 +2,52 @@ import pandas as pd
 from datetime import datetime
 
 class Bess:
-    def __init__(self, power_capacity = 100, energy_capacity = 200, efficiency = 0.9):
+    """
+    Class that represents a Battery Energy Storage System (BESS).
+    
+    
+    """
+     
+    def __init__(self, power_capacity: float = 100, energy_capacity: float = 200, efficiency: float = 0.9):
+        """
+        Parameters:
+        
+        power_capacity : maximum power output capacity to the system.
+        energy_capacity : nominal energy storage capacity available to the system.
+        efficiency : efficiency of the BESS (default: 0.9).
+        """
 
-        self.efficiency = efficiency
-        self.energy_capacity = energy_capacity / self.efficiency
-        self.power_capacity = power_capacity
-        self.schedule = None
 
-    def get_efficiency(self):
+        self.efficiency = efficiency                                #efficiency
+        self.energy_capacity = energy_capacity / self.efficiency    # energy capacity adjusted by efficiency
+        self.power_capacity = power_capacity                        # power capacity
+        self.schedule = None                                        # hourly schedule
+
+    def get_efficiency(self) -> float:
+        """
+        Returns the efficiency of the BESS.
+        """
         return self.efficiency
     
-    def get_energy_capacity(self):
+    def get_energy_capacity(self) ->float:
+        """
+        Returns the energy capacity of the BESS.
+        """
         return self.energy_capacity
     
-    def get_power_capacity(self):
+    def get_power_capacity(self) -> float:
+        """
+        Returns the power capacity of the BESS.
+        """
         return self.power_capacity
 
-    def set_schedule(self, schedule):
+    def set_schedule(self, schedule: pd.DataFrame):
+        """
+        Sets the optimal hourly schedule for the BESS.
+
+        Parameters:
+        schedule : Optimal Schedule to assign to the BESS.
+        """
         self.schedule = schedule
     
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,24 @@
 import pandas as pd
 
-def date_to_timezone(dataset, col_name, timezone = 'US/Central'):
-    ''' 
-    given a dataset, and a column (col_name) with datetime values,
-    convert it to timezone.
-    '''
+def date_to_timezone(dataset: pd.DataFrame, col_name: str, timezone: str = 'US/Central') -> pd.DataFrame:
+    """
+    Converts a datetime column in a dataset to a specified timezone.
+
+    Parameters:
+    dataset: The dataset containing the datetime column to be converted.
+    col_name: The name of the column with datetime values to be processed.
+    timezone: The timezone to localize the datetime values to. Default is 'US/Central'.
+
+    Returns: A DataFrame with the datetime column localized to the specified timezone.
+    zone.
+    """
+    # Check the column is datetime 
     dataset[col_name] = pd.to_datetime(dataset[col_name])
-        
+
+    # Set col_name as the index and localize to the timezone
     dataset = dataset.set_index(col_name).tz_localize(timezone, ambiguous='infer')
+
+    # Reset index back to normal column
     dataset = dataset.reset_index()
+
     return dataset
